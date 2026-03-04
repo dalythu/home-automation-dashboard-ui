8a713a7bfaef6d178724d3ec78d5571ece8d4d7a
dalythu <dalythu@gmail.com>
2026-03-04 15:27:01 -0500

    Map rooms list

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,3 +1,22 @@
 export default function Rooms() {
-    return <h1>Rooms</h1>
+
+  const rooms = [
+    { id: "living-room", name: "Living Room" },
+    { id: "kitchen", name: "Kitchen" },
+    { id: "bedroom", name: "Bedroom" },
+    { id: "bathroom", name: "Bathroom" }
+  ];
+
+  return (
+    <div>
+      <h1>Rooms</h1>
+
+      {rooms.map((room) => (
+        <div key={room.id}>
+          {room.name}
+        </div>
+      ))}
+
+    </div>
+  );
 }
