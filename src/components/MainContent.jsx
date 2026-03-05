b1d66ba9b75190cf9fd18c42178def87a1268358
dalythu <dalythu@gmail.com>
2026-03-05 13:12:39 -0500

    add room detail route and dynamic roomId page. Also removed current room for confusion

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Rooms from "../pages/Rooms";
 import Automations from "../pages/Automations";
-import CurrentRoom from "../pages/CurrentRoom";
+import RoomDetails from "../pages/RoomDetails";
 
 export default function MainContent() {
   return (
@@ -14,7 +14,7 @@ export default function MainContent() {
       <Route path="/rooms" element={<Rooms />} />
       <Route path="/automations" element={<Automations />} />
 
-      <Route path="/room/:roomId" element={<CurrentRoom />} />
+      <Route path="/rooms/:roomId" element={<RoomDetails />} />
     </Routes>
   );
 }
--- src/pages/CurrentRoom.jsx
+++ src/pages/CurrentRoom.jsx
@@ -1,3 +1,12 @@
-export default function CurrentRoom() {
-    return <h1>CurrentRoom</h1>
+import { useParams } from "react-router-dom";
+
+export default function RoomDetails() {
+  const { roomId } = useParams();
+
+  return (
+    <div>
+      <h1>Room Details</h1>
+      <p>Room ID: {roomId}</p>
+    </div>
+  );
 }
